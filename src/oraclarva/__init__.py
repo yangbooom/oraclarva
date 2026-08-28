874932b7f4782aba5cb7a6af9e0e1ef3cbc67987
Youngbum Kim <kimuyb@gmail.com>
2026-08-28 11:02:10 +0900

    Export surface and neuromuscular APIs

--- src/oraclarva/__init__.py
+++ src/oraclarva/__init__.py
@@ -3,13 +3,30 @@
 from .lif import LIFConfig, SparseLIFNetwork, Synapse
 from .body import BodyModelSpec, load_body_spec
 from .body3d import ScientificBody3D, Vec3
+from .neuromuscular import (
+    MuscleActivationFrame,
+    MuscleChannel,
+    MotorProjection,
+    NeuromuscularMap,
+    UnvalidatedMappingError,
+    load_neuromuscular_map,
+)
+from .surface import SurfaceMesh, build_surface_mesh
 
 __all__ = [
     "BodyModelSpec",
     "LIFConfig",
+    "MuscleActivationFrame",
+    "MuscleChannel",
+    "MotorProjection",
+    "NeuromuscularMap",
     "ScientificBody3D",
     "SparseLIFNetwork",
     "Synapse",
+    "SurfaceMesh",
+    "UnvalidatedMappingError",
     "Vec3",
+    "build_surface_mesh",
     "load_body_spec",
+    "load_neuromuscular_map",
 ]
